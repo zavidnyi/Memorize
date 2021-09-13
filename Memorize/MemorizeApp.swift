93e804f27cd822da7315b786e05d001e08fa4d30
ilya.zavidny@gmail.com <ilya.zavidny@gmail.com>
2021-09-13 12:17:41 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ilya Zavidny on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
